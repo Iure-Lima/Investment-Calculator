afa65bc5e3ac2871525d89f986ccd87002ac0320
IureLima <iurelimasilva@gmail.com>
2025-10-28 16:26:49 -0300

    feat: add the component for header

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,13 @@
+import "./Header.css"
+import Logo from "../../assets/investment-calculator-logo.png"
+
+function Header(){
+  return <>
+    <header>
+      <img src={Logo} alt="Investment Calculator Log" />
+      <h1>Investment Calculator</h1>
+    </header>
+  </>
+}
+
+export default Header;
